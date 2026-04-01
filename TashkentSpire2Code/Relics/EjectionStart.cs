4730c8121dadfce4542cb9712e044b01d5657351
FaFa-magic <1583275049@qq.com>
2026-04-01 19:41:08 +0800

    26.4.1.2
    遗物描述修改

--- TashkentSpire2Code/Relics/EjectionStart.cs
+++ TashkentSpire2Code/Relics/EjectionStart.cs
@@ -9,23 +9,17 @@ namespace TashkentSpire2.TashkentSpire2Code.Relics;
 
 public sealed class EjectionStart : TashkentRelic
 {
-    // 设为初始遗物
     public override RelicRarity Rarity => RelicRarity.Starter;
 
     // 大图标（通常用于查看遗物大图和通关结算）
     protected override string BigIconPath => 
-        "res://IndomitableSpire2/images/relics/big/ShikikanDakimakura.png";
+        "res://TashkentSpire2/images/relics/big/ShikikanDakimakura.png";
     // 小图标和轮廓图的路径
     public override string PackedIconPath => 
-        "res://IndomitableSpire2/images/relics/packed/ShikikanDakimakura_packed.tres";
+        "res://TashkentSpire2/images/relics/packed/ShikikanDakimakura_packed.tres";
     protected override string PackedIconOutlinePath => 
-        "res://IndomitableSpire2/images/relics/outline/ShikikanDakimakura_outline.tres";
-
-    // 定义动态变量：恢复 1 点生命值。参考了原版燃烧之血的写法。
-    // protected override IEnumerable<DynamicVar> CanonicalVars => 
-    //     [new HealVar(1M), new PowerVar<MotivationPower>(10M)];
-
-    // 在回合结束时恢复 1 点生命值。
+        "res://TashkentSpire2/images/relics/outline/ShikikanDakimakura_outline.tres";
+    
     public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
     {
         // 如果角色已经死亡，直接跳过（参考了燃烧之血的防崩溃处理）
