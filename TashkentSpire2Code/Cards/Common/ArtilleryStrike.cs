2f7aa66a6d640ab0ab0a79976eeac6eb8c6a9bcf
aofa li <1583275049@qq.com>
2026-04-06 08:49:10 +0800

    手模更新

--- TashkentSpire2Code/Cards/Common/ArtilleryStrike.cs
+++ TashkentSpire2Code/Cards/Common/ArtilleryStrike.cs
@@ -1,6 +1,31 @@
-﻿namespace TashkentSpire2.TashkentSpire2Code.Cards.Basics;
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 
-public class ArtilleryStrike
+namespace TashkentSpire2.TashkentSpire2Code.Cards.Common;
+
+public class ArtilleryStrike() : TashkentCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
 {
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
     
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(3m, ValueProp.Move),
+        new RepeatVar(4)
+    ];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, nameof(cardPlay.Target));
+        
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(DynamicVars.Repeat.IntValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+    
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(3M);
 }
---
+++ TashkentSpire2Code/DynamicVar/AmmunitionDynamicVar.cs
@@ -0,0 +1,16 @@
+﻿using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace TashkentSpire2.TashkentSpire2Code;
+
+public class AmmunitionDynamicVar : DynamicVar
+{
+    public const string Key = "Tashkent_Ammunition";
+
+    public static readonly string LocKey = Key.ToUpperInvariant();
+
+    public AmmunitionDynamicVar(decimal baseValue) : base(Key, baseValue)
+    {
+        this.WithTooltip(LocKey);
+    }
+}
