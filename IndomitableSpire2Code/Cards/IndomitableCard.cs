08ee642ab3ea959ee01886b0073038c106c8bdd3
BestNaonao <3375579162@qq.com>
2026-04-06 20:27:20 +0800

    Remove Unnecessary Classes.

--- IndomitableSpire2Code/Cards/IndomitableCard.cs
+++ IndomitableSpire2Code/Cards/IndomitableCard.cs
@@ -15,5 +15,16 @@ public abstract class IndomitableCard(
     bool autoAdd = true
 ) : CustomCardModel(baseCost, type, rarity, target, showInCardLibrary, autoAdd)
 {
-    
+    // Image size:
+    // Normal art: 1000x760 (Using 500x380 should also work, it will simply be scaled.)
+    // Full art: 606x852
+    // public override string CustomPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+    //
+    // Smaller variants of card images for efficiency:
+    // Smaller variant of fullart: 250x350
+    // Smaller variant of normalart: 250x190
+    //
+    // Uses card_portraits/card_name.png as image path. These should be smaller images.
+    // public override string PortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+    // public override string BetaPortraitPath => $"beta/{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
 }
