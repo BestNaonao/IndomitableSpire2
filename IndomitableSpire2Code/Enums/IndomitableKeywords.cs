8700c8c7727c15e0c595c73699f075245557292d
BestNaonao <3375579162@qq.com>
2026-04-13 02:31:40 +0800

    Workaround: Make CustomEnum Names Lowercase to Avoid BaseLib Analyzer Bug.

--- IndomitableSpire2Code/Enums/IndomitableKeywords.cs
+++ IndomitableSpire2Code/Enums/IndomitableKeywords.cs
@@ -6,30 +6,30 @@ namespace IndomitableSpire2.IndomitableSpire2Code.Enums;
 public static class IndomitableKeywords
 {
     // 舰载机总称
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("carrier_aircraft"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword CarrierAircraft;
 
     // 战斗机
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("fighter"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword Fighter;
 
     // 攻击机
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("attacker"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword Attacker;
 
     // 鱼雷轰炸机
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("torpedo_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword TorpedoBomber;
 
     // 俯冲轰炸机
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("dive_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword DiveBomber;
 
     // 水平轰炸机
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("level_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword LevelBomber;
 
     // 跳弹轰炸机
-    [CustomEnum, KeywordProperties(AutoKeywordPosition.Before)]
+    [CustomEnum("skip_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword SkipBomber;
 }
