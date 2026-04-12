b7da58d24f68977c1cc4d132370c568cc8b2be26
BestNaonao <3375579162@qq.com>
2026-04-12 23:10:25 +0800

    Add a Extension to Get Monsters' Attack Intent Info.

---
+++ IndomitableSpire2Code/Extensions/MonsterExtensions.cs
@@ -0,0 +1,45 @@
+﻿using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.MonsterMoves.Intents;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Extensions;
+
+public static class MonsterExtensions
+{
+    /// <summary>
+    /// 尝试获取怪物当前的攻击意图（自动兼容单次、多段、致死打击）
+    /// </summary>
+    public static AttackIntent? GetAttackIntent(this MonsterModel monster)
+    {
+        // 过滤并获取第一个攻击相关的意图
+        return monster.NextMove.Intents.OfType<AttackIntent>().FirstOrDefault();
+    }
+
+    /// <summary>
+    /// 获取怪物当前意图的单次攻击伤害（已计算力量、虚弱、易伤等最终面板伤害）
+    /// 如果怪物当前不打算攻击，返回 0。
+    /// </summary>
+    public static int GetIntentSingleDamage(this MonsterModel monster)
+    {
+        var attackIntent = monster.GetAttackIntent();
+        return attackIntent?.GetSingleDamage(monster.CombatState.PlayerCreatures, monster.Creature) ?? 0;
+    }
+
+    /// <summary>
+    /// 获取怪物当前意图的攻击段数。
+    /// 如果是单次攻击返回 1，多段攻击返回真实段数，不攻击返回 0。
+    /// </summary>
+    public static int GetIntentHitCount(this MonsterModel monster)
+    {
+        var attackIntent = monster.GetAttackIntent();
+        return attackIntent?.Repeats ?? 0;
+    }
+
+    /// <summary>
+    /// 获取怪物当前意图的总伤害（单次伤害 * 攻击段数）。
+    /// </summary>
+    public static int GetIntentTotalDamage(this MonsterModel monster)
+    {
+        var attackIntent = monster.GetAttackIntent();
+        return attackIntent?.GetTotalDamage(monster.CombatState.PlayerCreatures, monster.Creature) ?? 0;
+    }
+}
