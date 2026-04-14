2120b0b573d1f194bef165d914532fe93fbf1212
BestNaonao <3375579162@qq.com>
2026-04-14 22:44:43 +0800

    Add a Keyword, Formation.

--- IndomitableSpire2Code/Enums/IndomitableKeywords.cs
+++ IndomitableSpire2Code/Enums/IndomitableKeywords.cs
@@ -8,30 +8,34 @@ public static class IndomitableKeywords
     // 舰载机总称
     [CustomEnum("carrier_aircraft"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword CarrierAircraft;
-
+    
     // 战斗机
     [CustomEnum("fighter"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword Fighter;
-
+    
     // 攻击机
     [CustomEnum("attacker"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword Attacker;
-
+    
     // 鱼雷轰炸机
     [CustomEnum("torpedo_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword TorpedoBomber;
-
+    
     // 俯冲轰炸机
     [CustomEnum("dive_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword DiveBomber;
-
+    
     // 水平轰炸机
     [CustomEnum("level_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword LevelBomber;
-
+    
     // 跳弹轰炸机
     [CustomEnum("skip_bomber"), KeywordProperties(AutoKeywordPosition.Before)]
     public static CardKeyword SkipBomber;
+    
+    // 编队：在卡牌末尾显示，代表小队增援机制
+    [CustomEnum("formation"), KeywordProperties(AutoKeywordPosition.After)]
+    public static CardKeyword Formation;
     
     // 耐久度
     [CustomEnum("durability"), KeywordProperties(AutoKeywordPosition.Before)]
