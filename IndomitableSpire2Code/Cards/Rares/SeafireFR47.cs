c532ee7237b82e14c1aa4abf30254c3677b37fac
BestNaonao <3375579162@qq.com>
2026-04-18 22:01:21 +0800

    Add 2 Cards Designed based on Seafire.

---
+++ IndomitableSpire2Code/Cards/Rares/SeafireFR47.cs
@@ -0,0 +1,65 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Enums;
+using IndomitableSpire2.IndomitableSpire2Code.Commands;
+using IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Rares;
+
+public sealed class SeafireFr47() : CarrierAircraftCard(2, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+{
+    // 机体稍加固，飞行极度平稳
+    protected override int MaxDurability { get; set; } = 6;
+    protected override int UpgradeDurabilityAmount { get; set; } = 2;
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [IndomitableKeywords.StrikeFighter];
+    protected override IEnumerable<CardTag> SubclassTags => [IndomitableTags.StrikeFighter];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<VulnerablePower>()];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        ..base.CanonicalVars,
+        new DamageVar(6M, ValueProp.Move),
+        new RepeatVar(2), // 致敬共轴反转螺旋桨
+        new PowerVar<VulnerablePower>(2M),
+        new ReconVar(3M) // 搭载侦察变量
+    ];
+    
+    protected override async Task<IEnumerable<DamageResult>?> OnAircraftPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, nameof(cardPlay.Target));
+        
+        var attackCmd = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(DynamicVars.Repeat.IntValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .Execute(choiceContext);
+        
+        if (cardPlay.Target is { IsAlive: true })
+            await PowerCmd.Apply<VulnerablePower>(
+                target: cardPlay.Target,
+                amount: DynamicVars["VulnerablePower"].BaseValue,
+                applier: Owner.Creature,
+                cardSource: this
+            );
+        
+        // 核心机制：呼叫战区侦察
+        await ReconCmd.Execute(choiceContext, Owner, DynamicVars["Recon"].IntValue);
+        return attackCmd.Results;
+    }
+    
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2M);
+        DynamicVars["Recon"].UpgradeValueBy(2M); // 升级后侦察深度变强
+        UpgradeDurability();
+    }
+}
---
+++ IndomitableSpire2Code/Cards/Uncommons/SeafireF46.cs
@@ -0,0 +1,66 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Enums;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Uncommons;
+
+public sealed class SeafireF46() : CarrierAircraftCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    // 初始耐久极低
+    protected override int MaxDurability { get; set; } = 5;
+    protected override int UpgradeDurabilityAmount { get; set; } = 2;
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [IndomitableKeywords.StrikeFighter];
+    protected override IEnumerable<CardTag> SubclassTags => [IndomitableTags.StrikeFighter];
+    
+    // 在悬浮窗额外显示截击的能力提示
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<InterceptedPower>()];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        ..base.CanonicalVars,
+        new DamageVar(9M, ValueProp.Move),
+        new BlockVar(7M, ValueProp.Move),
+        new PowerVar<InterceptedPower>(5M) // 基础截击扣除 3 点力量
+    ];
+    
+    protected override async Task<IEnumerable<DamageResult>?> OnAircraftPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, nameof(cardPlay.Target));
+        
+        var attackCmd = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        
+        // 如果目标意图攻击，迎面施加截击削弱其火力
+        if (cardPlay.Target is { IsAlive: true, Monster.IntendsToAttack: true })
+            await PowerCmd.Apply<InterceptedPower>(
+                target: cardPlay.Target,
+                amount: DynamicVars["InterceptedPower"].BaseValue,
+                applier: Owner.Creature,
+                cardSource: this
+            );
+        else
+            // 掩护：获得格挡
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        
+        return attackCmd.Results;
+    }
+    
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3M);
+        DynamicVars.Block.UpgradeValueBy(3M);
+        DynamicVars["InterceptedPower"].UpgradeValueBy(2M);
+        UpgradeDurability();
+    }
+}
