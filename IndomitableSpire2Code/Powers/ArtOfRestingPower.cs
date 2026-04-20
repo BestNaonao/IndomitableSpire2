ecd925cdd6c91f5f130468bfae311b664591d3b9
BestNaonao <3375579162@qq.com>
2026-04-20 20:39:45 +0800

    Add a Art of Resting Power.

---
+++ IndomitableSpire2Code/Powers/ArtOfRestingPower.cs
@@ -0,0 +1,31 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Tokens;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Powers;
+
+public sealed class ArtOfRestingPower : IndomitablePower
+{
+    public override PowerType Type => PowerType.Buff;
+    
+    // 该能力无需堆叠层数，有与无的区别即可
+    public override PowerStackType StackType => PowerStackType.Single;
+    
+    // 核心钩子：在生成卡牌后拦截
+    public override async Task AfterCardGeneratedForCombat(CardModel card, bool addedByPlayer)
+    {
+        // 如果不是玩家生成的、不是状态牌、或者不是属于该角色的，直接放行
+        if (!addedByPlayer || card.Type != CardType.Status || card.Owner.Creature != Owner || Owner.Player == null)
+            return;
+        
+        // 闪烁并等待一下
+        Flash();
+        await Cmd.Wait(0.2f);
+        // 生成一张“养神”，并将生成的状态牌转化为它
+        var refreshCard = CombatState.CreateCard<Refresh>(Owner.Player);
+        await CardCmd.Transform(card, refreshCard);
+        await Cmd.Wait(0.2f);
+    }
+}
