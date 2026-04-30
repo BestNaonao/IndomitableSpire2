a27e6b8a2029655732f0d99b779d536333c53b0a
BestNaonao <3375579162@qq.com>
2026-05-01 01:14:56 +0800

    Add a Abstract Temporary Power Model in Project.

---
+++ IndomitableSpire2Code/Abstracts/IndomitableTemporaryPower.cs
@@ -0,0 +1,11 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using IndomitableSpire2.IndomitableSpire2Code.Extensions;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Abstracts;
+
+public abstract class IndomitableTemporaryPower : CustomTemporaryPowerModel
+{
+    public override string CustomBigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+    public override string CustomPackedIconPath => $"packed/{Id.Entry.RemovePrefix().ToLowerInvariant()}_packed.tres".PowerImagePath();
+}
