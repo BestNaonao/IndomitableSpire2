1fa396407dce6fe6fe2e7ecf78bed55c8ea8e216
BestNaonao <3375579162@qq.com>
2026-05-02 03:16:01 +0800

    Add Several Card Extensions and Dynamic Variable Set Extensions.

---
+++ IndomitableSpire2Code/Extensions/CardExtensions.cs
@@ -0,0 +1,15 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Models;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Extensions;
+
+public static class CardExtensions
+{
+    public static bool HasEnoughMotivation(this CardModel card) =>
+        card.HasEnoughMotivation(card.DynamicVars.MotivationConsume().IntValue);
+    
+    public static bool HasEnoughMotivation(this CardModel card, int amount) =>
+        card.CombatState != null && 
+        card.Owner.Creature.GetPower<MotivationPower>() is PowerModel power && 
+        power.DisplayAmount >= amount;
+}
---
+++ IndomitableSpire2Code/Extensions/DynamicVarSetExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Extensions;
+
+public static class DynamicVarSetExtensions
+{
+    public static DynamicVar Durability(this DynamicVarSet  vars) =>vars[nameof(Durability)];
+    
+    public static DynamicVar MaxDurability(this DynamicVarSet  vars) => vars[nameof(MaxDurability)];
+    
+    public static DynamicVar MotivationRequire(this DynamicVarSet  vars) => vars[nameof(MotivationRequire)];
+    
+    public static DynamicVar MotivationConsume(this DynamicVarSet  vars) => vars[nameof(MotivationConsume)];
+}
