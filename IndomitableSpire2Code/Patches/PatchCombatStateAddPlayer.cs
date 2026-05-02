072af490819580f9053fa8074a70caef7076ca88
BestNaonao <3375579162@qq.com>
2026-05-02 23:37:48 +0800

    Refact: Apply 0 Motivation Power at the Begin of Combat by Incresing and Decreasing 1 Stack.

--- IndomitableSpire2Code/Patches/PatchCombatStateAddPlayer.cs
+++ IndomitableSpire2Code/Patches/PatchCombatStateAddPlayer.cs
@@ -3,6 +3,7 @@ using IndomitableSpire2.IndomitableSpire2Code.Character;
 using IndomitableSpire2.IndomitableSpire2Code.Powers;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Helpers;
 
@@ -19,6 +20,16 @@ public static class PatchCombatStateAddPlayer
         if (targetCreature.IsDead) return;
         MainFile.Logger.Info("Harmony Patch: Indomitable entered combat!");
         // 同样使用 TaskHelper 来执行异步命令
-        TaskHelper.RunSafely(PowerCmd.Apply<MotivationPower>(targetCreature, 1, targetCreature, null));
+        TaskHelper.RunSafely(InitMotivation(targetCreature));
+    }
+    
+    // 5. 将无声的 +1 / -1 组合封装在异步方法中
+    private static async Task InitMotivation(Creature targetCreature)
+    {
+        // 先 +1，触发 AfterApplied 自动补齐底数，此时 Amount=2, 显示=1
+        await PowerCmd.Apply<MotivationPower>(targetCreature, 1, targetCreature, null, silent: true);
+        
+        // 再 -1，触发正常的递减，此时 Amount=1, 显示=0。完美达到拥有能力图标且数字为 0 的状态！
+        await PowerCmd.Apply<MotivationPower>(targetCreature, -1, targetCreature, null, silent: true);
     }
 }
