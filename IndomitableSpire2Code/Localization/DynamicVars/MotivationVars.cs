9e2456e42e5d0702e0f3ddfacdd8609cce5b4fd0
BestNaonao <3375579162@qq.com>
2026-05-02 13:31:48 +0800

    Add Motivation Variables.

---
+++ IndomitableSpire2Code/Localization/DynamicVars/MotivationVars.cs
@@ -0,0 +1,11 @@
+﻿using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
+
+// 干劲需求变量 (比如：需要 50 点干劲)
+public sealed class MotivationRequireVar(decimal baseValue)
+    : DynamicVar("MotivationRequire", baseValue);
+
+// 干劲消耗变量 (比如：消耗 25 点干劲)
+public sealed class MotivationConsumeVar(decimal baseValue) 
+    : DynamicVar("MotivationConsume", baseValue);
