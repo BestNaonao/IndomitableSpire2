bdcb287147a260328e08e6f3ba12b0becbf8260e
BestNaonao <3375579162@qq.com>
2026-05-03 12:05:50 +0800

    Remove Unused and Unnecessary AfterDynamicVarAmountChanged Hook.

--- IndomitableSpire2Code/Hooks/CustomHook.cs
+++ IndomitableSpire2Code/Hooks/CustomHook.cs
@@ -1,7 +1,5 @@
 ﻿using IndomitableSpire2.IndomitableSpire2Code.Abstracts;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
-using MegaCrit.Sts2.Core.Models;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Hooks;
 
@@ -20,27 +18,6 @@ public static class CustomHook
                 await subscriber.AfterEnergyGained(player, amount);
                 
                 // 【核心细节】：必须调用 InvokeExecutionFinished()，这是 STS2 引擎内部管理状态机的必要步骤
-                model.InvokeExecutionFinished();
-            }
-        }
-    }
-
-    public static async Task AfterDynamicVarAmountChanged(
-        AbstractModel sourceModel,
-        string variableName,
-        decimal originalAmount,
-        decimal offsetAmount,
-        Creature target,
-        Creature? applier)
-    {
-        if (target.CombatState == null) return;
-        
-        // 使用原版的 IterateHookListeners 来遍历当前战斗中所有合法的监听器模型
-        foreach (var model in target.CombatState.IterateHookListeners())
-        {
-            if (model is IAfterDynamicVarAmountChangedSubscriber subscriber)
-            {
-                await subscriber.AfterDynamicVarAmountChanged(sourceModel, variableName, originalAmount, offsetAmount, target, applier);
                 model.InvokeExecutionFinished();
             }
         }
