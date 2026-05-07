2b5100f29b83b01e1d123210d0077574bc84cb9b
BestNaonao <3375579162@qq.com>
2026-05-07 13:17:51 +0800

    Refact: Make MotivationGainVar Inherit from PowerVar.

--- IndomitableSpire2Code/Localization/DynamicVars/MotivationVars.cs
+++ IndomitableSpire2Code/Localization/DynamicVars/MotivationVars.cs
@@ -1,10 +1,11 @@
-﻿using MegaCrit.Sts2.Core.Localization.DynamicVars;
+﻿using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
 
 // 干劲获取变量 (比如：获得 10 点干劲)
 public sealed class MotivationGainVar(decimal baseValue) 
-    : DynamicVar("MotivationGain", baseValue);
+    : PowerVar<MotivationPower>("MotivationGain", baseValue);
 
 // 干劲需求变量 (比如：需要 50 点干劲)
 public sealed class MotivationRequireVar(decimal baseValue)
