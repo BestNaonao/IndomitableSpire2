7ae51e6515f7c4093ba987774c3632010c1c0729
BestNaonao <3375579162@qq.com>
2026-05-07 23:30:15 +0800

    Add a CustomPowerVar Using WithPowerTooltip Extension and MotivationVars Inherit.

---
+++ IndomitableSpire2Code/Localization/DynamicVars/CustomPowerVar.cs
@@ -0,0 +1,21 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Extensions;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
+
+// 自定义能力变量，可以自动添加提示框
+public class CustomPowerVar<T> : PowerVar<T> where T : PowerModel
+{
+    public static readonly string DefaultName = typeof(T).Name;
+    
+    public CustomPowerVar(decimal baseValue) : base(DefaultName, baseValue)
+    {
+        this.WithPowerTooltip<CustomPowerVar<T>, T>();
+    }
+    
+    public CustomPowerVar(string name, decimal baseValue) : base(name, baseValue)
+    {
+        this.WithPowerTooltip<CustomPowerVar<T>, T>();
+    }
+}
--- IndomitableSpire2Code/Localization/DynamicVars/MotivationVars.cs
+++ IndomitableSpire2Code/Localization/DynamicVars/MotivationVars.cs
@@ -1,24 +1,15 @@
-﻿using IndomitableSpire2.IndomitableSpire2Code.Extensions;
-using IndomitableSpire2.IndomitableSpire2Code.Powers;
-using MegaCrit.Sts2.Core.Localization.DynamicVars;
+﻿using IndomitableSpire2.IndomitableSpire2Code.Powers;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
 
 // 干劲获取变量 (比如：获得 10 点干劲)
-public sealed class MotivationGainVar : PowerVar<MotivationPower>
-{
-    public const string DefaultName = "MotivationGain";
-    
-    public MotivationGainVar(decimal baseValue) : base(DefaultName, baseValue)
-    {
-        this.WithPowerTooltip<MotivationGainVar, MotivationPower>();
-    }
-}
+public sealed class MotivationGainVar(decimal baseValue) 
+    : CustomPowerVar<MotivationPower>("MotivationGain", baseValue);
 
 // 干劲需求变量 (比如：需要 50 点干劲)
 public sealed class MotivationRequireVar(decimal baseValue)
-    : DynamicVar("MotivationRequire", baseValue);
+    : CustomPowerVar<MotivationPower>("MotivationRequire", baseValue);
 
 // 干劲消耗变量 (比如：消耗 25 点干劲)
 public sealed class MotivationConsumeVar(decimal baseValue) 
-    : DynamicVar("MotivationConsume", baseValue);
+    : CustomPowerVar<MotivationPower>("MotivationConsume", baseValue);
