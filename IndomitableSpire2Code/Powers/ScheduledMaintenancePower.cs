6360dfad25ee1a791bf46f0fce39cbbbb4409a7e
BestNaonao <3375579162@qq.com>
2026-05-12 23:57:25 +0800

    Add a Scheduled Maintenance Power.

---
+++ IndomitableSpire2Code/Powers/ScheduledMaintenancePower.cs
@@ -0,0 +1,21 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Powers;
+
+public sealed class ScheduledMaintenancePower : IndomitablePower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != Owner.Player || Amount <= 0) return;
+        
+        Flash();
+        // 调用我们编写的递归指令，传入当前的层数作为恢复池
+        await RepairCmd.RandomRepair(player, Amount);
+    }
+}
