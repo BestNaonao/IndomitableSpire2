49b87a61dad7fa8ebef1f23e97b8d51882ffd0c5
BestNaonao <3375579162@qq.com>
2026-05-15 21:26:53 +0800

    Refact: Replace the Preview Method used.

--- IndomitableSpire2Code/Cards/Others/EnhancedAPAmmo.cs
+++ IndomitableSpire2Code/Cards/Others/EnhancedAPAmmo.cs
@@ -6,10 +6,13 @@ using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Nodes;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Others;
 
@@ -40,7 +43,7 @@ public sealed class EnhancedApAmmo() : IndomitableSpire2Card(0, CardType.Skill, 
                  )
         {
             CardCmd.Enchant<ArmorPiercingEnchantment>(card, 1M);
-            CardCmd.Preview(card);
+            NRun.Instance?.GlobalUi.CardPreviewContainer.AddChildSafely(NCardEnchantVfx.Create(card)!);
         }
     }
     
