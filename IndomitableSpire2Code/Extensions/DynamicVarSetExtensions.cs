4a3a2cfdfb4c78ca4c2e193a845242ade01706b6
BestNaonao <3375579162@qq.com>
2026-05-17 23:50:49 +0800

    Add a Shield Method and a Aviation Method.

--- IndomitableSpire2Code/Extensions/DynamicVarSetExtensions.cs
+++ IndomitableSpire2Code/Extensions/DynamicVarSetExtensions.cs
@@ -19,6 +19,8 @@ public static class DynamicVarSetExtensions
     
     public static DynamicVar ArmorBreak(this DynamicVarSet  vars) => vars[nameof(ArmorBreakPower)];
     
+    public static DynamicVar Aviation(this DynamicVarSet  vars) => vars[nameof(AviationPower)];
+    
     public static DynamicVar Flooding(this DynamicVarSet  vars) => vars[nameof(FloodingPower)];
     
     public static DynamicVar OnFire(this DynamicVarSet  vars) => vars[nameof(OnFirePower)];
@@ -29,5 +31,7 @@ public static class DynamicVarSetExtensions
     
     public static DynamicVar Slow(this DynamicVarSet  vars) => vars[nameof(SlowPower)];
     
-    public static DynamicVar Recon(this DynamicVarSet  vars) => vars[ReconVar.DefaultName];
+    public static ReconVar Recon(this DynamicVarSet  vars) => (ReconVar) vars[ReconVar.DefaultName];
+    
+    public static ShieldVar Shield(this DynamicVarSet  vars) => (ShieldVar) vars[ShieldVar.DefaultName];
 }
