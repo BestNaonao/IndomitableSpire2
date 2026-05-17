2be564c17f3d785b26c397c6abe7ff05aab41a74
BestNaonao <3375579162@qq.com>
2026-05-17 23:57:44 +0800

    Add a ArmouredCarrier Card Using ArmouredCarrierPower.

---
+++ IndomitableSpire2Code/Cards/Rares/ArmouredCarrier.cs
@@ -0,0 +1,36 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Rares;
+
+public sealed class ArmouredCarrier() : IndomitableCard(3, CardType.Power, CardRarity.Rare, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new CustomPowerVar<ShieldPower>(6M),
+        new CustomPowerVar<AviationPower>(20M),
+        new PowerVar<ArmouredCarrierPower>(2M)
+    ];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        // 施加装甲航母能力，1 层代表 3 点护盾和 10 点航空的基础收益
+        await PowerCmd.Apply<ArmouredCarrierPower>(
+            target: Owner.Creature,
+            amount: DynamicVars["ArmouredCarrierPower"].BaseValue,
+            applier: Owner.Creature,
+            cardSource: this
+        );
+    }
+    
+    protected override void OnUpgrade()
+    {
+        // 升级效果：费用 -1 (变成 2 费)
+        EnergyCost.UpgradeBy(-1);
+    }
+}
