f461a96b78eb8c5b357ead2475b467246eec0c82
BestNaonao <3375579162@qq.com>
2026-05-18 23:09:14 +0800

    Use WithPowerTooltip to Add Power Tip Automatically.

--- IndomitableSpire2Code/Localization/DynamicVars/ShieldVar.cs
+++ IndomitableSpire2Code/Localization/DynamicVars/ShieldVar.cs
@@ -1,4 +1,6 @@
-﻿using MegaCrit.Sts2.Core.Localization.DynamicVars;
+﻿using IndomitableSpire2.IndomitableSpire2Code.Extensions;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
@@ -14,12 +16,12 @@ public class ShieldVar : BlockVar
     // 默认构造函数，直接传入数值即可，已经自带提示栏
     public ShieldVar(decimal baseValue, ValueProp props) : base(DefaultName, baseValue, props)
     {
-        // this.WithTooltip();
+        this.WithPowerTooltip<ShieldPower>();
     }
     
     // 重载构造函数，保留原版高度的灵活性
     public ShieldVar(string name, decimal baseValue, ValueProp props) : base(name, baseValue, props)
     {
-        // this.WithTooltip();
+        this.WithPowerTooltip<ShieldPower>();
     }
 }
