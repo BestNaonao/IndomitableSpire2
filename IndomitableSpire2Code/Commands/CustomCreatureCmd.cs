8e182141f2aae842dab28a324db13b62d7e85946
BestNaonao <3375579162@qq.com>
2026-05-19 23:52:43 +0800

    Add a MeetElegance Method to Support Elegance Judgement.

--- IndomitableSpire2Code/Commands/CustomCreatureCmd.cs
+++ IndomitableSpire2Code/Commands/CustomCreatureCmd.cs
@@ -1,4 +1,6 @@
 ﻿using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
@@ -43,5 +45,26 @@ public static class CustomCreatureCmd
         Creature? applier = null)
     {
         return await GainShield(target, blockVar.BaseValue, blockVar.Props, cardPlay, applier);
+    }
+    
+    /// <summary>
+    /// 判断目标生物上一回合是否符合“优雅”的条件（即：在上一回合存在，且没有受到任何未被格挡的伤害）
+    /// </summary>
+    public static bool MeetElegance(this Creature creature)
+    {
+        var combatState = creature.CombatState;
+        if (combatState == null) return false;
+        
+        // 计算回合数，第一回合没有上一回合，按规则无法触发优雅
+        var lastRound = combatState.RoundNumber - 1;
+        if (lastRound <= 0) return false;
+        
+        var tookUnblockedDamage = CombatManager.Instance.History.Entries
+            .OfType<DamageReceivedEntry>()
+            .Any(e => 
+                e.RoundNumber == lastRound && 
+                e.Receiver == creature && 
+                e.Result.UnblockedDamage > 0);
+        return !tookUnblockedDamage;
     }
 }
