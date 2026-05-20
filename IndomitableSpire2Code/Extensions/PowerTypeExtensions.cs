becf1c5013c04d5af56596e53d822228e9c738ea
BestNaonao <3375579162@qq.com>
2026-05-20 13:41:40 +0800

    Add Power Type Extensions to Get Inverted Power Type.

---
+++ IndomitableSpire2Code/Extensions/PowerTypeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using MegaCrit.Sts2.Core.Entities.Powers;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Extensions;
+
+public static class PowerTypeExtensions
+{
+    /// <summary>
+    /// 反转能力的极性：Buff 变 Debuff，Debuff 变 Buff，None 保持不变。
+    /// </summary>
+    public static PowerType Invert(this PowerType type) => type switch
+    {
+        PowerType.Buff => PowerType.Debuff,
+        PowerType.Debuff => PowerType.Buff,
+        _ => type
+    };
+    
+    /// <summary>
+    /// 根据条件决定是否反转能力的极性。
+    /// </summary>
+    public static PowerType InvertIf(this PowerType type, bool condition) => 
+        condition ? type.Invert() : type;
+}
