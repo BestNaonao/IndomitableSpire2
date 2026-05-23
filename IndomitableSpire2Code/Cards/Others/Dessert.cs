84290788ad1289ebec45a6a14dd6e2033a97cde0
BestNaonao <3375579162@qq.com>
2026-05-23 22:17:26 +0800

    Add a Dessert Card.

---
+++ IndomitableSpire2Code/Cards/Others/Dessert.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Others;
+
+[Pool(typeof(TokenCardPool))]
+public sealed class Dessert() : IndomitableSpire2Card(0, CardType.Skill, CardRarity.Token, TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.ForEnergy(this)];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new HealVar(3M), new EnergyVar(1)];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        // 1. 回复生命值
+        await CreatureCmd.Heal(Owner.Creature, DynamicVars.Heal.BaseValue);
+        
+        // 2. 获得能量
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
+    }
+    
+    protected override void OnUpgrade()
+    {
+        // 甜点升级后：回复的生命值 +1，获得的能量值 +1
+        DynamicVars.Heal.UpgradeValueBy(1M);
+        DynamicVars.Energy.UpgradeValueBy(1M);
+    }
+}
