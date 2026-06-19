0383af53a77f004f10f006bf60494656cb545f57
BestNaonao <3375579162@qq.com>
2026-06-19 15:34:10 +0800

    Add a Abstract Power Class with Auto Dynamic Variable Synchronization.

---
+++ IndomitableSpire2Code/Abstracts/DynamicVarSyncPower.cs
@@ -0,0 +1,21 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Abstracts;
+
+public abstract class DynamicVarSyncPower : IndomitablePower
+{
+    // 留给子类实现的抽象方法，用于定义如何根据当前层数同步动态变量
+    protected abstract void SyncDynamicVars();
+    
+    // 统一接管正式版的新签名钩子，加入 PlayerChoiceContext 参数
+    public override Task AfterPowerAmountChanged(
+        PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        // 确保触发的是当前能力自身的层数变化
+        if (power == this) SyncDynamicVars();
+        return Task.CompletedTask;
+    }
+}
