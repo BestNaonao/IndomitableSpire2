292f93fcf536064b16d5e0899686810bed46f6d6
BestNaonao <3375579162@qq.com>
2026-06-30 23:57:33 +0800

    Add a Second Hangar Power and Card.

---
+++ IndomitableSpire2Code/Cards/Uncommons/SecondHangar.cs
@@ -0,0 +1,34 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Uncommons;
+
+public sealed class SecondHangar() : IndomitableCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    // 注册能力变量：初始提供 3 层“第二机库”
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<SecondHangarPower>(3M)];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        // 播放施法动画
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        
+        // 赋予玩家“第二机库”能力
+        await PowerCmd.Apply<SecondHangarPower>(
+            choiceContext: choiceContext, 
+            target: Owner.Creature, 
+            amount: DynamicVars["SecondHangarPower"].BaseValue, 
+            applier: Owner.Creature, 
+            cardSource: this);
+    }
+    
+    protected override void OnUpgrade()
+    {
+        // 升级后：增加 1 层，即总共增加 4 张手牌上限
+        DynamicVars["SecondHangarPower"].UpgradeValueBy(1M);
+    }
+}
---
+++ IndomitableSpire2Code/Powers/SecondHangarPower.cs
@@ -0,0 +1,19 @@
+﻿using BaseLib.Hooks;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Powers;
+
+// 继承 IndomitablePower，并实现 BaseLib 的手牌上限修改接口
+public sealed class SecondHangarPower : IndomitablePower, IMaxHandSizeModifier
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter; 
+    
+    // 实现接口：修改手牌上限
+    public int ModifyMaxHandSize(Player player, int currentMaxHandSize)
+    {
+        // 确保这个钩子只对拥有该能力的玩家生效。直接加到当前上限上 Amount
+        return player != Owner.Player ? currentMaxHandSize : currentMaxHandSize + Amount;
+    }
+}
