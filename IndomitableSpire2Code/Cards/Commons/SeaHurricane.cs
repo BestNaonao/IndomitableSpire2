9a1214625eb65f5a98c61cadae2f0d62d3c96d9a
BestNaonao <3375579162@qq.com>
2026-07-15 22:46:13 +0800

    Add Sea Hurricane and Sea Hurricane Tac R Cards.

---
+++ IndomitableSpire2Code/Cards/Commons/SeaHurricane.cs
@@ -0,0 +1,77 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Enums;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Commons;
+
+public sealed class SeaHurricane() : CarrierAircraftCard(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override int MaxDurability { get; set; } = 6;
+    protected override int UpgradeDurabilityAmount { get; set; } = 2;
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [IndomitableKeywords.StrikeFighter];
+    protected override IEnumerable<CardTag> SubclassTags => [IndomitableTags.StrikeFighter];
+ 
+    private decimal _extraDamage;
+    private decimal ExtraDamage
+    {
+        get => _extraDamage;
+        set
+        {
+            AssertMutable();
+            _extraDamage = value;
+        }
+    }
+    
+    protected override IEnumerable<DynamicVar> AdditionalVars => [new DamageVar(12M, ValueProp.Move)];
+    
+    protected override async Task<IEnumerable<IEnumerable<DamageResult>>?> OnAircraftPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, nameof(cardPlay.Target));
+        
+        var attackCmd = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_heavy_blunt")
+            .Execute(choiceContext);
+        
+        return attackCmd.Results;
+    }
+    
+    // 核心机制：当拥有者被攻击时，增加本局游戏内的伤害
+    public override Task AfterDamageReceived(
+        PlayerChoiceContext choiceContext,
+        Creature target,
+        DamageResult result,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
+    {
+        // 确保是真正的攻击
+        if (target != Owner.Creature || dealer == null || !props.IsPoweredAttack() || CombatState == null)
+            return Task.CompletedTask;
+        
+        DynamicVars.Damage.BaseValue += 2M;
+        ExtraDamage += 2M;
+        return Task.CompletedTask;
+    }
+    
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(4M);
+        UpgradeDurability();
+    }
+    
+    // 重写降级钩子，确保局内成长不会丢失
+    protected override void AfterDowngraded()
+    {
+        base.AfterDowngraded();
+        DynamicVars.Damage.BaseValue += ExtraDamage;
+    }
+}
---
+++ IndomitableSpire2Code/Cards/Commons/SeaHurricaneTacR.cs
@@ -0,0 +1,61 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Commands;
+using IndomitableSpire2.IndomitableSpire2Code.Enums;
+using IndomitableSpire2.IndomitableSpire2Code.Extensions;
+using IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Commons;
+
+public sealed class SeaHurricaneTacR() : CarrierAircraftCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override int MaxDurability { get; set; } = 5;
+    protected override int UpgradeDurabilityAmount { get; set; } = 2;
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [IndomitableKeywords.StrikeFighter];
+    protected override IEnumerable<CardTag> SubclassTags => [IndomitableTags.StrikeFighter];
+    
+    protected override IEnumerable<DynamicVar> AdditionalVars =>
+    [
+        new DamageVar(4M, ValueProp.Move),
+        new ReconVar(2M),
+        new CustomPowerVar<AirRaidGuidancePower>(5M)
+    ];
+    
+    protected override async Task<IEnumerable<IEnumerable<DamageResult>>?> OnAircraftPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, nameof(cardPlay.Target));
+        
+        var attackCmd = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .Targeting(cardPlay.Target)
+            .Execute(choiceContext);
+        
+        // 侦察
+        await ReconCmd.Execute(choiceContext, Owner, DynamicVars.Recon().IntValue);
+        
+        // 赋予“目标引导”能力
+        await PowerCmd.Apply<AirRaidGuidancePower>(
+            choiceContext, 
+            Owner.Creature, 
+            DynamicVars.AirRaidGuidance().BaseValue, 
+            Owner.Creature, 
+            this);
+        
+        return attackCmd.Results;
+    }
+    
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2M);
+        DynamicVars.Recon().UpgradeValueBy(1M);
+        DynamicVars.AirRaidGuidance().UpgradeValueBy(2M);
+        UpgradeDurability();
+    }
+}
