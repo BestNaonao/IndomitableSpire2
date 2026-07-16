0bc3c6de1fce48c281f02053d8716c03259b6b42
BestNaonao <3375579162@qq.com>
2026-07-16 23:30:06 +0800

    Balance: Buff Yoga Card by Adding Motivation Gaining.

--- IndomitableSpire2Code/Cards/Uncommons/Yoga.cs
+++ IndomitableSpire2Code/Cards/Uncommons/Yoga.cs
@@ -1,4 +1,6 @@
 ﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Extensions;
+using IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
 using IndomitableSpire2.IndomitableSpire2Code.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
@@ -10,7 +12,11 @@ namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Uncommons;
 public sealed class Yoga() : IndomitableCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
     // 初始提供 2 层瑜伽能力（即 2 点护盾）
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<YogaPower>(2M)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new PowerVar<YogaPower>(2M), 
+        new MotivationGainVar(10M)
+    ];
     
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
@@ -24,11 +30,21 @@ public sealed class Yoga() : IndomitableCard(1, CardType.Skill, CardRarity.Uncom
             Owner.Creature, 
             this
         );
+        
+        // 获得干劲
+        await PowerCmd.Apply<MotivationPower>(
+            choiceContext: choiceContext, 
+            target: Owner.Creature, 
+            amount: DynamicVars.MotivationGain().BaseValue, 
+            applier: Owner.Creature, 
+            cardSource: this
+        );
     }
     
     protected override void OnUpgrade()
     {
         // 升级后变为 3 层
         DynamicVars["YogaPower"].UpgradeValueBy(1M);
+        DynamicVars.MotivationGain().UpgradeValueBy(5M);
     }
 }
