f5c85083a170a957f9e27d7a1ad7c9df48b2e14d
BestNaonao <3375579162@qq.com>
2026-08-17 21:03:39 +0800

    Refact: Move IndomitableSkin out and Assign Special Resources with Skin Manager.

--- IndomitableSpire2Code/Character/Indomitable.cs
+++ IndomitableSpire2Code/Character/Indomitable.cs
@@ -10,28 +10,21 @@ using MegaCrit.Sts2.Core.Models;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Character;
 
-public enum IndomitableSkin
-{
-    Default,
-    Maid
-}
-
 // 【核心】：必须是 abstract，该类只是玩法数据的容器，不是一个具体角色
 public abstract class Indomitable : CustomCharacterModel
 {
     public const string CharacterId = "Indomitable";
     public override CharacterGender Gender => CharacterGender.Feminine;
-    internal static readonly Color TopicColor = new("FFFFFF");
-    
-    public abstract IndomitableSkin CurrentSkin { get; }
-    
-    // 角色名称的颜色，STS2 使用 Godot 的 Color 结构体
-    public override Color NameColor => TopicColor; // 替换为不挠的主题色，比如白色或淡蓝
-    
     public override int StartingHp => 69; // 初始血量
     public override int StartingGold => 99; // BaseLib 默认是 99，你可以重写修改
     
-    // 小地图、对话、能量球相关的颜色设置
+    internal static readonly Color TopicColor = new("FFFFFF");
+    
+    public abstract IndomitableSkin CurrentSkin { get; }
+    protected SkinDefinition CurrentSkinDefinition => SkinManager.GetDefinition(CurrentSkin);
+    
+    // 角色名称等的颜色，STS2 使用 Godot 的 Color 结构体
+    public override Color NameColor => TopicColor; // 替换为不挠的主题色，比如白色或淡蓝
     public override Color EnergyLabelOutlineColor => new Color("1E283CFF");
     public override Color DialogueColor => new("AAAAAA");
     public override Color RemoteTargetingLineColor => new("AAAAAA");
@@ -59,7 +52,7 @@ public abstract class Indomitable : CustomCharacterModel
     
     public override IReadOnlyList<RelicModel> StartingRelics => [ModelDb.Relic<ShikikanDakimakura>()];
     
-    // ... 同样的方式替换选人界面的立绘、头像等 ...
+    // 全局通用的皮肤资产
     public override string CustomCharacterSelectBg =>               // 选择界面背景
         $"res://IndomitableSpire2/scenes/characters/char_select_bg_indomitable_{new Random().Next(1, 5)}.tscn";
     public override string CustomCharacterSelectIconPath =>         // 选择界面图标
@@ -74,6 +67,20 @@ public abstract class Indomitable : CustomCharacterModel
         "res://IndomitableSpire2/scenes/combat/energy_counters/indomitable_energy_counter.tscn";
     public override string CustomTrailPath =>                       // 卡牌轨迹特效
         "res://IndomitableSpire2/scenes/vfx/card_trail_indomitable.tscn";
+    
+    // 每个皮肤的特色资源
+    public override Color MapDrawingColor => CurrentSkinDefinition.MapDrawingColor;
+    public override string CustomVisualPath => CurrentSkinDefinition.VisualPath;
+    public override string CustomRestSiteAnimPath => CurrentSkinDefinition.RestSiteAnimPath;
+    public override string CustomMerchantAnimPath => CurrentSkinDefinition.MerchantAnimPath;
+    public override string CustomIconTexturePath => CurrentSkinDefinition.IconTexturePath;
+    public override string CustomIconPath => CurrentSkinDefinition.IconPath;
+    public override string CustomIconOutlineTexturePath => CurrentSkinDefinition.IconOutlineTexturePath;
+    public override string CustomArmPaperTexturePath => CurrentSkinDefinition.ArmPaperTexturePath;
+    public override string CustomArmPointingTexturePath => CurrentSkinDefinition.ArmPointingTexturePathFunc();
+    public override string CustomArmRockTexturePath => CurrentSkinDefinition.ArmRockTexturePath;
+    public override string CustomArmScissorsTexturePath => CurrentSkinDefinition.ArmScissorsTexturePath;
+    public override string CharacterSelectSfx => CurrentSkinDefinition.CharacterSelectSfx;
     
     // 原版逻辑构建动作映射，传入 Spine 文件中实际命名的动作字符串
     public override CreatureAnimator SetupCustomAnimationStates(MegaSprite controller) => SetupAnimationState(
