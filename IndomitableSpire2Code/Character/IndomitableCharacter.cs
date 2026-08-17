e54d4d937cfcf05d5cd004696d22da6fb4d964d9
BestNaonao <3375579162@qq.com>
2026-08-17 21:07:42 +0800

    Migrate, Rename, Refact Indomitable Sub-Characters and Add new Race Queen Character.

--- IndomitableSpire2Code/Character/IndomitableCharacter.cs
+++ IndomitableSpire2Code/Character/IndomitableCharacter.cs
@@ -1,45 +1,37 @@
-﻿using Godot;
+﻿using MegaCrit.Sts2.Core.Models;
 
 namespace IndomitableSpire2.IndomitableSpire2Code.Character;
 
-public sealed class IndomitableCharacter : Indomitable
+public sealed class IndomitableDefault : Indomitable
 {
-    public override Color MapDrawingColor => Colors.FloralWhite;
-    
-    // 逻辑控制开关：当前选择的皮肤。
-    // 设置为 static 方便以后在 UI 界面（如 CharacterSelectScreen）的按钮点击事件中直接修改：
+    // 现在只需要重写 CurrentSkin，父类就会自动从 SkinManager 中读取子类对应的皮肤定义的各项特色资源
     public override IndomitableSkin CurrentSkin => IndomitableSkin.Default;
+}
+
+public sealed class IndomitableMaid : Indomitable
+{
+    // 现在只需要重写 CurrentSkin，父类就会自动从 SkinManager 中读取子类对应的皮肤定义的各项特色资源
+    public override IndomitableSkin CurrentSkin => IndomitableSkin.Maid;
     
-    // 根据 CurrentSkin 动态获取 CustomVisualPath，指向 Godot 导出的角色视觉场景 (tscn) 包路径
-    public override string CustomVisualPath => 
-        "res://IndomitableSpire2/scenes/characters/indomitable.tscn";
-    // 获取休息点的视觉场景
-    public override string CustomRestSiteAnimPath => 
-        "res://IndomitableSpire2/scenes/characters/indomitable_rest_site.tscn";
-    // 获取商店的视觉场景
-    public override string CustomMerchantAnimPath => 
-        "res://IndomitableSpire2/scenes/merchant/indomitable_merchant.tscn";
-    // 小图标
-    public override string CustomIconTexturePath => 
-        "res://IndomitableSpire2/images/charui/character_icon_indomitable.png";
-    // 角色图标场景
-    public override string CustomIconPath => 
-        "res://IndomitableSpire2/scenes/characters/indomitable_icon.tscn";
-    // 角色图标轮廓
-    public override string CustomIconOutlineTexturePath => 
-        "res://IndomitableSpire2/images/ui/character_icon_indomitable_outline.png";
+    public override bool HideFromVanillaCharacterSelect => true;
     
-    // 人物手模图片(石头剪刀布和指向)
-    public override string CustomArmPaperTexturePath => 
-        "res://IndomitableSpire2/images/charui/hands/multiplayer_hand_indomitable_paper.png";
-    public override string CustomArmPointingTexturePath => 
-        $"res://IndomitableSpire2/images/charui/hands/multiplayer_hand_indomitable_point{new Random().Next(1, 4)}.png";
-    public override string CustomArmRockTexturePath => 
-        "res://IndomitableSpire2/images/charui/hands/multiplayer_hand_indomitable_rock.png";
-    public override string CustomArmScissorsTexturePath => 
-        "res://IndomitableSpire2/images/charui/hands/multiplayer_hand_indomitable_scissors.png";
+    public override bool HideInCompendium => true;
     
-    // 人物选择时播放的台词
-    public override string CharacterSelectSfx => 
-        "res://IndomitableSpire2/sfx/characters/indomitable/chumotaici_ex.wav";
+    public override bool AllowInVanillaRandomCharacterSelect => true;
+    
+    public override ModelId DefaultCompendiumOpenModelId => ModelDb.Character<IndomitableDefault>().Id;
+}
+
+public sealed class IndomitableRaceQueen : Indomitable
+{
+    // 现在只需要重写 CurrentSkin，父类就会自动从 SkinManager 中读取子类对应的皮肤定义的各项特色资源
+    public override IndomitableSkin CurrentSkin => IndomitableSkin.RaceQueen;
+    
+    public override bool HideFromVanillaCharacterSelect => true;
+    
+    public override bool HideInCompendium => true;
+    
+    public override bool AllowInVanillaRandomCharacterSelect => true;
+    
+    public override ModelId DefaultCompendiumOpenModelId => ModelDb.Character<IndomitableDefault>().Id;
 }
