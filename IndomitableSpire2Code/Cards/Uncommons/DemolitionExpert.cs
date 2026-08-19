a2ad96e28866c6b4a0add1d84a4f1a57e7a26c8a
BestNaonao <3375579162@qq.com>
2026-08-19 23:38:05 +0800

    Add a Demolition Expert Card and a Furious Card.

---
+++ IndomitableSpire2Code/Cards/Uncommons/DemolitionExpert.cs
@@ -0,0 +1,39 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Uncommons;
+
+public sealed class DemolitionExpert() : IndomitableCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    // 注册变量：能力层数 1
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+        [new PowerVar<DemolitionExpertPower>(1M)];
+    
+    // 提供悬浮提示
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
+        [HoverTipFactory.FromPower<OnFirePower>(), HoverTipFactory.FromPower<FloodingPower>()];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        
+        await PowerCmd.Apply<DemolitionExpertPower>(
+            choiceContext: choiceContext, 
+            target: Owner.Creature, 
+            amount: DynamicVars["DemolitionExpertPower"].BaseValue, 
+            applier: Owner.Creature, 
+            cardSource: this
+        );
+    }
+    
+    protected override void OnUpgrade()
+    {
+        // 升级效果：层数增加量 +1
+        DynamicVars["DemolitionExpertPower"].UpgradeValueBy(1M);
+    }
+}
---
+++ IndomitableSpire2Code/Cards/Uncommons/Furious.cs
@@ -0,0 +1,49 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
+using IndomitableSpire2.IndomitableSpire2Code.Extensions;
+using IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
+using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Cards.Uncommons;
+
+public sealed class Furious() : IndomitableCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    // 注册变量：抽 1 张牌，固定 4 层起火
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new CardsVar(1),
+        new CustomPowerVar<OnFirePower>(4M)
+    ];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        
+        // 1. 先给予自身“怒火中烧”能力
+        await PowerCmd.Apply<FuriousPower>(
+            choiceContext: choiceContext, 
+            target: Owner.Creature, 
+            amount: DynamicVars.Cards.BaseValue, 
+            applier: Owner.Creature, 
+            cardSource: this
+        );
+        
+        // 2. 给予自身 4 层起火。由于刚刚获得了能力，这一步会立刻触发第一抽！
+        await PowerCmd.Apply<OnFirePower>(
+            choiceContext: choiceContext, 
+            target: Owner.Creature, 
+            amount: DynamicVars.OnFire().BaseValue, 
+            applier: Owner.Creature, 
+            cardSource: this
+        );
+    }
+    
+    protected override void OnUpgrade()
+    {
+        // 升级效果：抽牌数量 +1
+        DynamicVars.Cards.UpgradeValueBy(1M);
+    }
+}
