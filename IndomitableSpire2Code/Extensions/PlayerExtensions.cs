267e472bbea0e21ee0d59dd5e03804f59da8e1dc
BestNaonao <3375579162@qq.com>
2026-08-29 23:27:46 +0800

    Add a PlayerExtension to Get Player's Motivation Amount Conveniently.

---
+++ IndomitableSpire2Code/Extensions/PlayerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IndomitableSpire2.IndomitableSpire2Code.Powers;
+using MegaCrit.Sts2.Core.Entities.Players;
+
+namespace IndomitableSpire2.IndomitableSpire2Code.Extensions;
+
+public static class PlayerExtensions
+{
+    /// <summary>
+    /// 获取玩家干劲的快捷方法
+    /// </summary>
+    public static int GetMotivationAmount(this Player player) => 
+        player.Creature.GetPower<MotivationPower>()?.DisplayAmount ?? 0;
+}
