86529c2ec8fdfc8b812c8f043f3c483b8d084151
BestNaonao <3375579162@qq.com>
2026-09-04 19:46:58 +0800

    Refactor Resolve Card to Apply Ethereal instead of Carrier Aircraft.

--- IndomitableSpire2Code/Cards/Others/Resolve.cs
+++ IndomitableSpire2Code/Cards/Others/Resolve.cs
@@ -1,6 +1,5 @@
 using BaseLib.Utils;
 using IndomitableSpire2.IndomitableSpire2Code.Cards.Abstracts;
-using IndomitableSpire2.IndomitableSpire2Code.Enums;
 using IndomitableSpire2.IndomitableSpire2Code.Extensions;
 using IndomitableSpire2.IndomitableSpire2Code.Localization.DynamicVars;
 using IndomitableSpire2.IndomitableSpire2Code.Powers;
@@ -20,8 +19,7 @@ public sealed class Resolve() : IndomitableSpire2Card(1, CardType.Status, CardRa
     
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain, CardKeyword.Exhaust];
     
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
-        [HoverTipFactory.FromKeyword(IndomitableKeywords.CarrierAircraft)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CardKeyword.Ethereal)];
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [new MotivationGainVar(10M)];
     
@@ -34,13 +32,12 @@ public sealed class Resolve() : IndomitableSpire2Card(1, CardType.Status, CardRa
             applier: Owner.Creature,
             cardSource: this
         );
-        // AllPiles 只包含手牌、抽牌堆、弃牌堆、消耗牌堆和打出牌堆，不包含局外的 Deck。当前正在结算的执念位于打出牌堆，因此也会获得舰载机关键词。
+        // AllPiles 只包含手牌、抽牌堆、弃牌堆、消耗牌堆和打出牌堆，不包含局外的 Deck。因此当前正在结算的执念也会获得虚无。
         if (Owner.PlayerCombatState is not { } playerCombatState) return;
         var resolveCards = playerCombatState.AllPiles
             .SelectMany(pile => pile.Cards)
             .OfType<Resolve>()
-            .Where(card => card != this)
             .ToList();
-        foreach (var resolveCard in resolveCards) CardCmd.ApplyKeyword(resolveCard, IndomitableKeywords.CarrierAircraft);
+        foreach (var resolveCard in resolveCards) CardCmd.ApplyKeyword(resolveCard, CardKeyword.Ethereal);
     }
 }
